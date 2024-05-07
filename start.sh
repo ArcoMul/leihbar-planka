aaa19778c1a247d1664e9d2239094b776115a23a
Arco Mul <arco.mul@denkwerk.com>
2024-05-07 13:28:45 +0200

    Make planka fly.io ready: add fly.toml and configure volume storage in start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+ln -s /mnt/fly-volume/user-avatars /app/public/user-avatars
+ln -s /mnt/fly-volume/project-background-images /app/public/project-background-images
+ln -s /mnt/fly-volume/attachments /app/private/attachments
+
 export NODE_ENV=production && set -e && node db/init.js && node app.js --prod
